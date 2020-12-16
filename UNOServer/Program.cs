0b6976b65ae2b047c56aff729c4a901630469dd5
adm-sabitovka <sabitovka@shkd.bizml.ru>
2020-12-16 12:14:55 +0300

    Добавлен проект клиенской версии

--- UNOServer/Program.cs
+++ UNOServer/Program.cs
@@ -1,23 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 using UNOServer.ServerObjects;
 
 namespace UNOServer {
     class Program {
-        static ServerObject server; // сервер
-        static Thread listenThread; // потока для прослушивания
+
         static TcpListener tcpListener;
+        static Thread waitThread;
+        static TcpClient mainClient;
+
         static void Main(string[] args) {
             try {
                 tcpListener = new TcpListener(IPAddress.Any, 8888);
                 tcpListener.Start();
                 Console.WriteLine("Сервер запущен. Ожидание подключений...");
+                // список клиентов для подключения
+                var lobby = new List<TcpClient>();
+                // клиент, который начинает игру
+
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                mainClient = tcpClient;
+
+                waitThread = new Thread(new ThreadStart(WaitUntilStart));
+                waitThread.Start();
 
                 while (true) {
-                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
-                    
+                    tcpClient = tcpListener.AcceptTcpClient();
+                    lobby.Add(tcpClient);
                     //ClientObject clientObject = new ClientObject(tcpClient, this);
                     //Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
                     //clientThread.Start();
@@ -25,6 +38,29 @@ namespace UNOServer {
             } catch (Exception ex) {
                 Console.WriteLine(ex.Message);
                 //Disconnect();
+            }
+        }
+
+        private static void WaitUntilStart() {
+            var stream = mainClient.GetStream();
+            while (true) {
+                try {
+                    byte[] data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message);//вывод сообщения
+                } catch {
+                    Console.WriteLine("Подключение прервано!"); //соединение было прервано
+                    Console.ReadLine();
+                    //Disconnect();
+                }
             }
         }
 
